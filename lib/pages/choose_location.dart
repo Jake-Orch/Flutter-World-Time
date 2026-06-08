9be115e6db3837b22cce3aba71f63bb7308ba9fb
Jake Orchard <jakeorchard@live.co.uk>
2026-06-08 20:30:35 +0100

    Add async getTime function

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,6 +8,7 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +25,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: Text('choose location screen'),
     );
   }
 }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,24 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    // make request
+    Response response = await get(Uri.parse('https://timeapi.io/api/Time/current/zone?timeZone=Europe/London'));
+    Map data = jsonDecode(response.body);
+
+    // get properties from data
+    DateTime now = DateTime.parse(data['dateTime']);
+    print(now);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
